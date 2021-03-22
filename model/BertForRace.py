72554852e0811d036d8268fe392e6bfc203274a3
NCJ <me@ncj.wiki>
2021-03-22 12:03:40 +0800

    reformat

--- model/BertForRace.py
+++ model/BertForRace.py
@@ -8,16 +8,17 @@ from data.RACEDataModule import RACEDataModule
 
 
 class BertForRace(pl.LightningModule):
-    def __init__(self,
-                 pretrained_model: str = 'bert-large-uncased',
-                 bert_config: str = 'bert-large-uncased',  # pretrained_model+'/bert_config.json'
-                 learning_rate: float = 2e-5,
-                 gradient_accumulation_steps: int = 1,
-                 num_train_epochs: float = 3.0,
-                 train_batch_size: int = 32,
-                 warmup_proportion: float = 0.1,
-                 train_all: bool = False,
-                 use_bert_adam: bool = True):
+    def __init__(
+            self,
+            pretrained_model: str = 'bert-large-uncased',
+            bert_config: str = 'bert-large-uncased',  # pretrained_model+'/bert_config.json'
+            learning_rate: float = 2e-5,
+            gradient_accumulation_steps: int = 1,
+            num_train_epochs: float = 3.0,
+            train_batch_size: int = 32,
+            warmup_proportion: float = 0.1,
+            train_all: bool = False,
+            use_bert_adam: bool = True):
         super().__init__()
         self.config = BertConfig.from_pretrained(bert_config, num_choices=4)
         print(self.config)
