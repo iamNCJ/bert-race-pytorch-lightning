b28874dbdac21f0bede8df12721a74fc5dd43afd
NCJ Charlie <me@ncj.wiki>
2021-04-13 16:24:55 +0800

    Update train.py

--- train.py
+++ train.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
         learning_rate=2e-5,
         num_train_epochs=20,
         train_batch_size=4,
-        train_all=Ture,
+        train_all=True,
         use_longformer=False,
     )
     dm = RACEDataModule(
