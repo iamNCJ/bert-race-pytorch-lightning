63f6f448abdd5b6247448609d8a028269b277cdd
NCJ Charlie <me@ncj.wiki>
2021-04-20 09:53:09 +0800

    lower learning rate

--- model/DCMNForRace.py
+++ model/DCMNForRace.py
@@ -85,9 +85,9 @@ class DCMNForRace(pl.LightningModule):
         optimizer_grouped_parameters = [
             {'params': [p for n, p in bert_param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
             {'params': [p for n, p in bert_param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0},
-            {'params': [p for n, p in classifer_param_optimizer], 'lr': 1e-4},
-            {'params': [p for n, p in ssmatch_param_optimizer], 'lr': 1e-4},
-            {'params': [p for n, p in fuse_param_optimizer], 'lr': 1e-4},
+            {'params': [p for n, p in classifer_param_optimizer]},
+            {'params': [p for n, p in ssmatch_param_optimizer]},
+            {'params': [p for n, p in fuse_param_optimizer]},
             # {'params': list(self.pooler.named_parameters()), 'lr': 1e-4},
         ]
         optimizer = AdamW(
