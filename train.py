98074761aac6f9d84f3553cff7490e2319ee1d64
NCJ Charlie <me@ncj.wiki>
2021-04-22 22:51:57 +0800

    even larger BS

--- train.py
+++ train.py
@@ -13,13 +13,13 @@ if __name__ == '__main__':
         pretrained_model='./model/bert-large-uncased',
         learning_rate=2e-5,
         num_train_epochs=6,
-        train_batch_size=16,
+        train_batch_size=32,
         train_all=True,
     )
     dm = RACEDataModule(
         model_name_or_path='./model/bert-large-uncased',
         datasets_loader='./data/RACELocalLoader.py',
-        train_batch_size=16,
+        train_batch_size=32,
         max_seq_length=128,
         num_workers=8,
         num_preprocess_processes=48,
